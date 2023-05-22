841522ce74212e54e87994145cc6a65183a497cd
salmanali.bh <salmanali@bh.edu.pk>
2023-05-22 09:24:17 +0500

    first commit

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -85,14 +85,14 @@ const getAllProduct = asyncHandler(async (req, res) => {
 
     // pagination
 
-    const page = req.query.page;
-    const limit = req.query.limit;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const productCount = await Product.countDocuments();
-      if (skip >= productCount) throw new Error("This Page does not exists");
-    }
+    // const page = req.query.page;
+    // const limit = req.query.limit;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+    // if (req.query.page) {
+    //   const productCount = await Product.countDocuments();
+    //   if (skip >= productCount) throw new Error("This Page does not exists");
+    // }
     const product = await query;
     res.json(product);
   } catch (error) {
